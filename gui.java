f1dd33d5f742750f163af0c79eff974b3b408a67
SAMV <samwv@icloud.com>
2025-02-03 17:07:23 +0100

    basic start screen

--- gui.java
+++ gui.java
@@ -1,3 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 public class gui {
-    
+    public gui() {
+        JFrame startFrame = new JFrame("Budget App Homescreen");
+        startFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        startFrame.setSize(500,400);
+        startFrame.setLayout(new BorderLayout());
+
+        JLabel titleLabel = new JLabel("Budget App");
+        titleLabel.setFont(new Font("Seriff", Font.BOLD, 24));
+        startFrame.add(titleLabel, BorderLayout.CENTER);
+
+        JButton startButton = new JButton("Start App");
+        startButton.setFont(new Font("Arial", Font.BOLD, 18));
+        startButton.setFocusPainted(false);
+
+        startButton.addActionListener(new ActionListener(){
+            @Override
+                public void actionPerformed(ActionEvent e) {
+                    JFrame gameFrame = new JFrame("Math Screen");
+                    gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    gameFrame.setSize(1000,1000);
+
+                    gameFrame.setVisible(true);
+                    startFrame.dispose();
+                }
+        });
+        startFrame.add(startButton, BorderLayout.SOUTH);
+        startFrame.setLocationRelativeTo(null);
+        startFrame.setVisible(true);
+
+    }
 }
